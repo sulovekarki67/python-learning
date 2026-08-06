372d0d0ec793859d901590d494083f667e9840a5
sulovekarki67 <sulovekarki67@gmail.com>
2026-08-06 21:49:37 +0545

    Add interactive dictionary and set
    Add two simple interactive examples under 'Dictinory and Sets':
    - dictonary.py: prompts for 4 friend names and their favorite languages, stores them in a dict and prints items.
    - sets.py: prompts for 8 integers, inserts them into a set, and prints one popped (unique) element.
    These are small demo scripts to illustrate basic dict and set usage (note: set.pop() returns an arbitrary element).

---
+++ Dictinory and Sets/dictonary.py
@@ -0,0 +1,6 @@
+data = {}
+data.update({input("1st friend enter your name"):input("1st friend enter your fav lang")})
+data.update({input("2nd friend enter your name"):input("2nd friend enter your fav lang")})
+data.update({input("3rd friend enter your name"):input("3rd friend enter your fav lang")})
+data.update({input("4th friend enter your name"):input("4th friend enter your fav lang")})
+print(data.items())
